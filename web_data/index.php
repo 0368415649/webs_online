36e18be1f4bb53e213a0453504e0680eba270630
Nguyễn Đình Duy <101943731+0368415649@users.noreply.github.com>
2022-03-26 09:25:58 +0700

    đã upload

--- web_data/index.php
+++ web_data/index.php
@@ -18,6 +18,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,152;0,500;1,200;1,300;1,400;1,500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="./assets/css/gird.css">   
@@ -229,7 +230,7 @@
 <div class="content">
                 <div class="gird wide color hiden">
                         <div class="row">
-                                <div class="col l-2 m-2 c-2 pr">
+                                <div class="col l-2 c-2 pr">
                                         <div class="gird2">
                                             <h3 class="gird2_font">Danh Mục Sản Phẩm</h3>
                                             <ul class="gird2_list">
@@ -245,9 +246,9 @@
                                     </div>
                             
 
-                                <div class="col l-10 m-10 c-10">
+                                <div class="col l-10  c-10">
                                             <div class="row">                                     
-                                                    <div class="gird10_header_left col l-12 m-12 c-12">
+                                                    <div class="gird10_header_left col l-12  c-12">
                                                             <ul class="gird10_header_left-list">   
                                                                 <li class="gird10_header_left-item gird10_none">Sắp xếp theo</li>
                                                                 <li class="gird10_header_left-item"><a class='gird10_header_left-item-link' href="">  Phổ biến</a>  </li>
@@ -269,7 +270,7 @@
                                                                 </ul>
 
                                                         </div>
-                                                    <div class="main col  l-12 m-12 c-12">
+                                                    <div class="main col  l-12  c-12">
                                                             <div class="row x">
                                                             </div>
                                                         </div>
@@ -301,10 +302,21 @@
                             <i class="fas fa-angle-right"></i>
                 </a> -->
         </div>
+        
+        <div class="b-example-divider"></div>
 
-       <footer class="footer">
-
-       </footer>
+        <div class="container">
+            <footer class="py-3 my-4 color">
+                <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+                <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Home</a></li>
+                <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Features</a></li>
+                <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Pricing</a></li>
+                <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">FAQs</a></li>
+                <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
+                </ul>
+                <p class="text-center text-muted">&copy; 2021 Company, Inc</p>
+            </footer>
+        </div>
 
    </div>
 
@@ -588,7 +600,10 @@ trousers.onclick = function(){
          </div>  
          </div>
         
-</body >
+         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    </body >
 
 </html>
 <script src="modal_login.js"></script>
